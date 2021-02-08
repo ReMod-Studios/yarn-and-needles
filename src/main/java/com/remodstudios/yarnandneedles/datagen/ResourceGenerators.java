b6bf581ae75de85fa85457ab726b0d9823b56b00
leocth <22406910+LeoCTH@users.noreply.github.com>
2021-02-08 11:09:24 +0800

    tools

--- src/main/java/com/remodstudios/yarnandneedles/datagen/ResourceGenerators.java
+++ src/main/java/com/remodstudios/yarnandneedles/datagen/ResourceGenerators.java
@@ -7,12 +7,14 @@ import com.remodstudios.yarnandneedles.datagen.generators.block.SimpleBlockGener
 import com.remodstudios.yarnandneedles.datagen.generators.block.TrapdoorBlockGenerator;
 import com.remodstudios.yarnandneedles.datagen.generators.item.BlockItemGenerator;
 import com.remodstudios.yarnandneedles.datagen.generators.item.SimpleItemGenerator;
+import net.minecraft.util.Identifier;
 
 public final class ResourceGenerators {
 
     public static final ResourceGenerator NO_OP = new NoOpResourceGenerator();
     public static final ResourceGenerator SIMPLE_BLOCK = new SimpleBlockGenerator();
     public static final ResourceGenerator SIMPLE_ITEM = new SimpleItemGenerator();
+    public static final ResourceGenerator HANDHELD_ITEM = new SimpleItemGenerator(new Identifier("item/handheld"));
     public static final ResourceGenerator BLOCK_ITEM = new BlockItemGenerator();
     public static final ResourceGenerator LOG_BLOCK = new LogBlockGenerator();
     public static final ResourceGenerator DOOR_BLOCK = new DoorBlockGenerator();
