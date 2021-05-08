a964c58a887c55440e6b295cbae655716c9eb706
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-05-08 21:20:55 +0300

    Make flat block item gen make more sense

--- src/main/java/com/remodstudios/yarnandneedles/datagen/generators/item/FlatBlockItemGenerator.java
+++ src/main/java/com/remodstudios/yarnandneedles/datagen/generators/item/FlatBlockItemGenerator.java
@@ -1,22 +1,22 @@
 package com.remodstudios.yarnandneedles.datagen.generators.item;
 
+import com.remodstudios.yarnandneedles.datagen.ResourceGenerator;
 import com.swordglowsblue.artifice.api.ArtificeResourcePack;
 import com.swordglowsblue.artifice.api.util.IdUtils;
 import net.minecraft.util.Identifier;
 
-public class FlatBlockItemGenerator extends BlockItemGenerator {
-    public FlatBlockItemGenerator(Identifier parentModelId) {
-        super(parentModelId);
-    }
+public class FlatBlockItemGenerator implements ResourceGenerator {
+    private static final Identifier PARENT_ID = new Identifier("item/generated");
 
-    public FlatBlockItemGenerator() {
-        super();
+    @Override
+    public void generateAssets(ArtificeResourcePack.ClientResourcePackBuilder pack, Identifier id) {
+        pack.addItemModel(id, model -> model
+                .parent(PARENT_ID)
+                .texture("layer0", IdUtils.wrapPath("block/", id)));
     }
 
     @Override
-    protected void generateItemModel(ArtificeResourcePack.ClientResourcePackBuilder pack, Identifier id) {
-        pack.addItemModel(id, model -> model
-                .parent(new Identifier("item/generated"))
-                .texture("layer0", IdUtils.wrapPath("block/", id)));
+    public void generateData(ArtificeResourcePack.ServerResourcePackBuilder pack, Identifier id) {
+
     }
 }
